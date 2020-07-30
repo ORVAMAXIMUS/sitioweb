57283763bc490e2565d90940a10baf0d24127b7b
roemoorva <69015744+roemoorva@users.noreply.github.com>
2020-07-30 16:31:30 -0500

    Update nav.php

---
+++ nav.php
@@ -0,0 +1,8 @@
+<header>
+  <ol>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">Quienes somo</a></li>
+    <li><a href="#">Nosotros</a></li>
+    <li><a href="#">Contacto</a></li>
+  </ol>
+</header>
